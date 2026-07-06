c28cde14eedde3dd7a186f3d459dcc788bd745f7
al-amin90 <ijesun30@gmail.com>
2026-07-06 20:59:19 +0600

    feat: Frontend Deploy url cors added

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,14 @@ const app: Application = express();
 // parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:5173"] }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://inventory-sales-management-frontend.vercel.app",
+    ],
+  }),
+);
 
 // all application route here
 app.use("/api/v1", router);
